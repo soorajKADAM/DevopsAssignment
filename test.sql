6637a1003a7adb8fea19ca3ad4ce1ed089a533be
soorajKADAM <soorajak102@gmail.com>
2022-01-11 20:25:17 +0530

    first commit

---
+++ test.sql
@@ -0,0 +1,11 @@
+CREATE TABLE studentData(
+	Name varchar(100),
+	RollNO int(10),
+	Address varchar(500),
+	MobileNumber int(11),
+	PanNumber varchar(15)
+);
+INSERT INTO studentData VALUES('Akhil',101,'Ahmedabad',7398546891,'gfc78ht12yi9');
+INSERT INTO studentData VALUES('James',102,'Pune',7394566891,'ytu78ht12yi9');
+INSERT INTO studentData VALUES('Yash',103,'Pune',8788546856,'gf48kjt12op8');
+INSERT INTO studentData VALUES('Sam',104,'Kolhapur',8956126123,'yui78ht1278p');
